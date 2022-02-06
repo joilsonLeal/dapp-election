1b96e537b84f3fd434319a28f113bc7626084d0f
Joilson Leal <joilson_rg@hotmail.com>
2022-02-06 16:59:15 -0300

    feat: update smart contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,28 @@
 pragma solidity >= 0.5.16 <= 0.8.11;
 
 contract Election {
-  string public candidate;
+
+  struct Candidate {
+    uint id;
+    string name;
+    uint vouteCount;
+  }
+
+  mapping(uint => Candidate) public candidates;
+
+  uint public candidateCount;
 
   constructor() public {
-    candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate(string memory _name) private {
+    candidateCount++;
+    candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+  }
+
+  function add(string memory _name) public {
+    candidates[1] = Candidate(1, _name, 0);
   }
 }
